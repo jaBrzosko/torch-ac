8865ff36a3953d54fae883135d6c116c077d4d8e
jaBrzosko <jakubbrzoskowski@gmail.com>
2025-11-27 17:27:52 +0100

    chore: specify types

--- torch_ac/utils/helper.py
+++ torch_ac/utils/helper.py
@@ -1,7 +1,7 @@
 import torch
 from torch.distributions.categorical import Categorical
 
-def mask_tensor(categorical, mask):
+def mask_tensor(categorical: Categorical, mask: torch.Tensor) -> Categorical:
     logits = categorical.logits
     masked_logits = logits + torch.log(mask.clip(min=1e-8))
 
